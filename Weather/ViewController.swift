19741e5707a0d282b777a3c81092096951d5718e
shotaro-ooe <shoooe97@gmail.com>
2019-07-21 12:37:37 +0900

    ライブラリの導入とATS設定完了

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by 大江祥太郎 on 2019/07/21.
+//  Copyright © 2019 shotaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
